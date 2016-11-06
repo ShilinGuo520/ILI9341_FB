224153e9d51a2c11bf2dccb3af625deda44c4bae
shilin.guo <552002499@qq.com>
2016-11-06 08:59:22 +0000

    Changes to be committed: 	modified:   update.sh
    Add Reference & make time

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Reference https://www.raspberrypi.org/documentation/linux/kernel/building.md
+
 echo "start"
+
+start_time=`date +%Y%m%d%H%M%S`
 
 #git clone --depth=1 https://github.com/raspberrypi/linux
 #sudo apt-get install bc
@@ -19,5 +23,18 @@ sudo cp arch/arm/boot/dts/overlays/README /boot/overlays/
 sudo scripts/mkknlimg arch/arm/boot/zImage /boot/$KERNEL.img
 
 echo "end"
+
+end_time=`date +%Y%m%d%H%M%S`
+
+
+echo $start_time 
+echo $end_time
+
+
+cd ..
+echo $start_time > update_time
+echo $end_time >> update_time
+echo `expr $time1 - $time` >> update_time
+
 sudo reboot
 
